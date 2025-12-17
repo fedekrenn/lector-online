c24a47268bdf8fb04a813993cc764bfd9295d4e4
Fede Krenn <fedekrenn@gmail.com>
2025-12-16 22:02:32 -0300

    refactor: Simplify configuration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
   adapter: vercel(),
-  integrations: [react(), tailwind()]
+  integrations: [react()],
 });
